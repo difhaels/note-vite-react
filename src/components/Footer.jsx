e70c28c00ae0ee8807b7a1598ba3d822fff21e43
Agung Saputra <agungsaputradifh@gmail.com>
2024-07-07 21:03:53 +0700

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
-export default function Footer({items}) {
+export default function Footer({ items }) {
     const totalList = [...items];
-  
-    if(totalList.length === 0) return <footer className="stats">Daftar Belanja Kosong</footer>
-  
+
+    if (totalList.length === 0)
+        return <footer className="stats">Daftar Belanja Kosong</footer>;
+
     const listChacked = totalList.filter((item) => item.checked === true);
-    const percent = Math.round((listChacked.length/totalList.length)*100);
-    
+    const percent = Math.round((listChacked.length / totalList.length) * 100);
+
     return (
-      <footer className="stats">
-        Ada {totalList.length} barang di daftar belanjaan, {listChacked.length} barang sudah dibeli ({percent}%)
-      </footer>
+        <footer className="stats">
+        Ada {totalList.length} barang di daftar belanjaan, {listChacked.length}{" "}
+        barang sudah dibeli ({percent}%)
+        </footer>
     );
-  }
+}
