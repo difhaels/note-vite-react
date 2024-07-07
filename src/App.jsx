18b6f7c0e0e875edc21930644ce0caefd70613e2
Agung Saputra <agungsaputradifh@gmail.com>
2024-07-07 19:04:55 +0700

    fitur toggle checked

--- src/App.jsx
+++ src/App.jsx
@@ -32,11 +32,15 @@ export default function App() {
     setItems((items) => items.filter((item) => item.id !== id))
   }
 
+  function handleToggleItem(id){
+    setItems((items) => items.map((item) => (item.id === id? {...item, checked: !item.checked} : item)));
+  }
+
   return (
     <div className="app">
       <Header />
       <Form onAddItem={handleAddItem} />
-      <GroceryList items={items} onDeleteItem={handleDeleteItem}/>
+      <GroceryList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/>
       <Footer />
     </div>
   );
@@ -99,13 +103,13 @@ function Form({onAddItem}) {
   );
 }
 
-function GroceryList({items, onDeleteItem}) {
+function GroceryList({items, onDeleteItem, onToggleItem}) {
   return (
     <>
       <div className="list">
         <ul>
           {items.map((item) => (
-            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
           ))}
         </ul>
       </div>
@@ -121,10 +125,10 @@ function GroceryList({items, onDeleteItem}) {
   );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
-      <input type="checkbox" />
+      <input type="checkbox" checked={item.checked} onChange={() => onToggleItem(item.id)}/>
       <span
         style={
           item.checked
